4633f294e0f9072a7079dd13af9a40e3c911197b
Steve Bour <bourpower@gmail.com>
2022-05-01 16:31:10 -0400

    addtnl calendar work

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -4,14 +4,32 @@ import "react-calendar/dist/Calendar.css";
 
 const Calendar = (props) => {
   const [value, onChange] = useState(new Date());
+
   function clicky() {
     console.log("in function");
+  }
+
+  function setAppt() {
+    console.log(value);
+    const choice = window.confirm(
+      `Please confirm your appointment choice for ${value}`
+    );
+    if (value < Date.now) {
+      window.alert("Selected date is out of range");
+    } else {
+      if (choice) {
+        console.log("the thing is done");
+        //reroute to home page, send confirmation email
+      } else {
+        console.log("the thing is not done");
+        //exit function
+      }
+    }
   }
 
   return (
     <div>
       <Calendarr
-        id="root"
         onChange={onChange}
         value={value}
         onClick={clicky()}
@@ -21,7 +39,7 @@ const Calendar = (props) => {
         <div>
           <h2>Available Appointments</h2>
           {props.availability.map((time) => (
-            <button key={props.availability.id} className="apptbtn">
+            <button key={props.availability.id} id="apptbtn" onClick={setAppt}>
               {time.time}
             </button>
           ))}
