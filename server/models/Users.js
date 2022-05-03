50b34b6af13a555c0a8cc2fba7c92f338f7a3030
brittany klose <brittany.klose@yahoo.com>
2022-05-02 21:26:28 -0400

    working on resolvers and typedefs

--- server/models/Users.js
+++ server/models/Users.js
@@ -17,7 +17,7 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
-  savedAppointments:[apptSchema],
+  savedAppts:[apptSchema],
 },
 {
   toJSON: {
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,27 +1,72 @@
-const { Tech, Matchup } = require('../models');
+const { AuthenticationError } = require("apollo-server-express");
+const {User, Stylists}= require('../models');
 
 const resolvers = {
   Query: {
-    tech: async () => {
-      return Tech.find({});
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('savedAppts');
     },
-    matchups: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return Matchup.find(params);
+    savedAppts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Appointment.find(params).sort({ createdAt: -1 });
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate("savedAppts");
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
   },
   Mutation: {
-    createMatchup: async (parent, args) => {
-      const matchup = await Matchup.create(args);
-      return matchup;
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
     },
-    createVote: async (parent, { _id, techNum }) => {
-      const vote = await Matchup.findOneAndUpdate(
-        { _id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
-        { new: true }
-      );
-      return vote;
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user found with this email");
+      }
+
+      const correctP = await user.isCorrectPassword(password);
+
+      if (!correctP) {
+        throw new AuthenticationError("Incorrect password or email");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveAppt: async (
+      parent,
+      { ApptId, client, stylist, service, scheduledFor },
+      context
+    ) => {
+      if (context.user) {
+        const appointment = await Appointment.create({
+          ApptId,
+          client,
+          stylist,
+          service,
+          scheduledFor,
+          
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedAppt: appointment._id } }
+        );
+    //how to save to stylist too. might have to figure out how to make them all users..
+        // await Stylist.findOneAndUpdate(
+        //   { _id: context.stylist._id },
+        //   { $addToSet: { savedAppt: appointment._id } }
+        // );
+        return appointment;
+      }
+      throw new AuthenticationError("You need to be logged in to book an appointment");
     },
   },
 };
