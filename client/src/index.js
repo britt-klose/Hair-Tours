2d982b89ca3138e7940a780228990e993793da13
Elizabeth Rodriguez <elizabethyoannie@outlook.com>
2022-05-03 21:58:36 -0400

    service menu

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { StyledEngineProvider } from '@mui/material/styles';
+import Demo from './demo';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+);
+
+ReactDOM.createRoot(document.querySelector("#root")).render(
+  <StyledEngineProvider injectFirst>
+    <Demo />
+  </StyledEngineProvider>
 );
