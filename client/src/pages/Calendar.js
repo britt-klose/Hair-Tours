fa4741015849a2ff46d04b2546a615c40d791e51
smeske10 <35307525+smeske10@users.noreply.github.com>
2022-05-05 21:40:47 -0400

    updated provider page and provider list map

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -1,70 +1,70 @@
-import React from "react";
-import CalendlyEventListener, { PopupWidget } from "react-calendly";
+// import React from "react";
+// import CalendlyEventListener, { PopupWidget } from "react-calendly";
 
-const Calendar = () => {
-  
-  function redirect() {
-    window.location.href= 'localhost:3000/checkout'
-  }
-  return (
-    <div>
-      <div className="App">
-        <PopupWidget
-          url="https://calendly.com/bourpower/hot-shave"
-          rootElement={document.getElementById("root")}
-          text="Click here to schedule!"
-          textColor="#ffffff"
-          color="#00a2ff"
-        />
-      </div>
-      <div className="App">
-        <PopupWidget
-          url="https://calendly.com/razorramona/hair-cut"
-          rootElement={document.getElementById("root")}
-          text="Click here to schedule!"
-          textColor="#ffffff"
-          color="#00a2ff"
-        />
-      </div>
-      <div className="App">
-        <PopupWidget
-          url="https://calendly.com/swanoldronson/beard-trim"
-          rootElement={document.getElementById("root")}
-          text="Click here to schedule!"
-          textColor="#ffffff"
-          color="#00a2ff"
-        />
-      </div>
-      <div className="App">
-        <PopupWidget
-          url="https://calendly.com/roygbiv0737/hair-coloring"
-          rootElement={document.getElementById("root")}
-          text="Click here to schedule!"
-          textColor="#ffffff"
-          color="#00a2ff"
-        />
-      </div>
-      <div className="App">
-        <PopupWidget
-          url="https://calendly.com/razorramona/waxing"
-          rootElement={document.getElementById("root")}
-          text="Click here to schedule!"
-          textColor="#ffffff"
-          color="#00a2ff"
-        />
-      </div>
-      <div className="App">
-        <PopupWidget
-          url="https://calendly.com/swanoldronson/styling"
-          rootElement={document.getElementById("root")}
-          text="Click here to schedule!"
-          textColor="#ffffff"
-          color="#00a2ff"
-        />
-        <CalendlyEventListener onEventScheduled={redirect} />
-      </div>
-    </div>
-  );
-};
+// const Calendar = () => {
 
-export default Calendar;
+//   function redirect() {
+//     window.location.href= 'localhost:3000/checkout'
+//   }
+//   return (
+//     <div>
+//       <div className="App">
+//         <PopupWidget
+//           url="https://calendly.com/bourpower/hot-shave"
+//           rootElement={document.getElementById("root")}
+//           text="Click here to schedule!"
+//           textColor="#ffffff"
+//           color="#00a2ff"
+//         />
+//       </div>
+//       <div className="App">
+//         <PopupWidget
+//           url="https://calendly.com/razorramona/hair-cut"
+//           rootElement={document.getElementById("root")}
+//           text="Click here to schedule!"
+//           textColor="#ffffff"
+//           color="#00a2ff"
+//         />
+//       </div>
+//       <div className="App">
+//         <PopupWidget
+//           url="https://calendly.com/swanoldronson/beard-trim"
+//           rootElement={document.getElementById("root")}
+//           text="Click here to schedule!"
+//           textColor="#ffffff"
+//           color="#00a2ff"
+//         />
+//       </div>
+//       <div className="App">
+//         <PopupWidget
+//           url="https://calendly.com/roygbiv0737/hair-coloring"
+//           rootElement={document.getElementById("root")}
+//           text="Click here to schedule!"
+//           textColor="#ffffff"
+//           color="#00a2ff"
+//         />
+//       </div>
+//       <div className="App">
+//         <PopupWidget
+//           url="https://calendly.com/razorramona/waxing"
+//           rootElement={document.getElementById("root")}
+//           text="Click here to schedule!"
+//           textColor="#ffffff"
+//           color="#00a2ff"
+//         />
+//       </div>
+//       <div className="App">
+//         <PopupWidget
+//           url="https://calendly.com/swanoldronson/styling"
+//           rootElement={document.getElementById("root")}
+//           text="Click here to schedule!"
+//           textColor="#ffffff"
+//           color="#00a2ff"
+//         />
+//         <CalendlyEventListener onEventScheduled={redirect} />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Calendar;
